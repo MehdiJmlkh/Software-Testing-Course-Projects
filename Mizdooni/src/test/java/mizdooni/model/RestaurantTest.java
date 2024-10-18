59c5527f8a84675b56ac173782f377ed19eb7d06
Mehdi <mehdijmlkh1382@gmail.com>
2024-10-18 21:07:43 +0330

    Test add table and get table

--- Mizdooni/src/test/java/mizdooni/model/RestaurantTest.java
+++ Mizdooni/src/test/java/mizdooni/model/RestaurantTest.java
@@ -1,11 +1,47 @@
 package mizdooni.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static mizdooni.utils.CreateSample.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RestaurantTest {
     private Restaurant restaurant;
 
+    @BeforeEach
     public void setup() {
         restaurant = create_sample_restaurant();
+    }
+
+    @Test
+    public void add_new_table_added_to_the_tables(){
+        Table table = create_sample_table();
+        restaurant.addTable(table);
+        assertEquals(1, restaurant.getTables().size());
+        assertEquals(table, restaurant.getTables().getFirst());
+    }
+
+    @Test
+    public void add_new_table_increases_table_number(){
+        Table table1 = create_sample_table();
+        Table table2 = create_sample_table();
+        restaurant.addTable(table1);
+        restaurant.addTable(table2);
+        assertEquals(1, table2.getTableNumber() - table1.getTableNumber());
+    }
+
+    @Test
+    public void get_existent_table_returns_the_table() {
+        Table table = create_sample_table();
+        restaurant.addTable(table);
+        assertEquals(table, restaurant.getTable(table.getTableNumber()));
+    }
+
+    @Test
+    public void get_non_existent_table_returns_null() {
+        Table table = create_sample_table();
+        restaurant.addTable(table);
+        assertNull(restaurant.getTable(table.getTableNumber() + 1));
     }
 }
