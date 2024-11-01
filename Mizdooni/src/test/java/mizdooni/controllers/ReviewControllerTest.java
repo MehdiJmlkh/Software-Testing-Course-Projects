6d9e949e59a5c52fc80224fb46358b240b5973f8
Mehdi <mehdijmlkh1382@gmail.com>
2024-11-01 09:11:19 +0330

    Complete tests of ReviewController

--- Mizdooni/src/test/java/mizdooni/controllers/ReviewControllerTest.java
+++ Mizdooni/src/test/java/mizdooni/controllers/ReviewControllerTest.java
@@ -44,22 +44,23 @@ public class ReviewControllerTest {
     }
 
     @Test
-    void getReviews_ValidRestaurantId_ReturnsReviews() throws RestaurantNotFound {
+    void getReviews_ExistedRestaurantId_ReturnsReviews() throws RestaurantNotFound {
         Restaurant restaurant = createSampleRestaurant();
         PagedList<Review> reviews = createSamplePageListOfReviews();
+        int page = createSamplePositiveNumber();
 
-        when(restaurantService.getRestaurant(anyInt())).thenReturn(restaurant);
-        when(reviewService.getReviews(anyInt(), anyInt())).thenReturn(reviews);
+        when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(restaurant);
+        when(reviewService.getReviews(restaurant.getId(), page)).thenReturn(reviews);
 
-        Response response = Response.ok("reviews for restaurant (" + restaurant.getId() + "): " + restaurant.getName(), reviews);
-        Response result = reviewController.getReviews(restaurant.getId(), 2);
+        Response result = reviewController.getReviews(restaurant.getId(), page);
 
-        assertEquals(result.getMessage(), response.getMessage());
-        assertEquals(result.getData(), response.getData());
+        assertEquals(HttpStatus.OK, result.getStatus());
+        assertEquals("reviews for restaurant (" + restaurant.getId() + "): " + restaurant.getName(), result.getMessage());
+        assertEquals(reviews, result.getData());
     }
 
     @Test
-    void getReviews_InvalidRestaurantId_ThrowsException() {
+    void getReviews_NotExistedRestaurantId_ThrowsException() {
         ResponseException responseException = new ResponseException(HttpStatus.NOT_FOUND, "restaurant not found");
         when(restaurantService.getRestaurant(anyInt())).thenThrow(responseException);
 
@@ -67,12 +68,57 @@ public class ReviewControllerTest {
             reviewController.getReviews(createSampleId(), createSamplePositiveNumber());
         });
 
-
         assertEquals(responseException, result);
     }
 
     @Test
-    void addReview_ParamsMissing_ThrowsException() {
+    public void addReview_ValidParams_addsReviews() throws UserNotFound, ManagerCannotReview, UserHasNotReserved, RestaurantNotFound, InvalidReviewRating {
+        Restaurant restaurant = createSampleRestaurant();
+        String comment = createSampleComment();
+        Map<String, Number> ratingMap = createSampleRatingMap();
+        Map<String, Object> params = Map.of("comment", comment,"rating", ratingMap);
+        when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(restaurant);
+
+        reviewController.addReview(restaurant.getId(), params);
+
+        verify(reviewService).addReview(eq(restaurant.getId()), argThat(rating ->
+                                rating.food == ratingMap.get("food").doubleValue() &&
+                                rating.service == ratingMap.get("service").doubleValue() &&
+                                rating.ambiance == ratingMap.get("ambiance").doubleValue() &&
+                                rating.overall == ratingMap.get("overall").doubleValue()),
+                            eq(comment));
+    }
+
+    @Test
+    public void addReview_ValidParams_ReturnsCorrectResponse() {
+        Restaurant restaurant = createSampleRestaurant();
+        String comment = createSampleComment();
+        Map<String, Number> ratingMap = createSampleRatingMap();
+        Map<String, Object> params = Map.of("comment", comment,"rating", ratingMap);
+        when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(restaurant);
+
+        Response response = reviewController.addReview(restaurant.getId(), params);
+
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals("review added successfully", response.getMessage());
+    }
+
+    @Test
+    void addReview_CommentMissing_ThrowsException() {
+        Restaurant restaurant = createSampleRestaurant();
+        when(restaurantService.getRestaurant(anyInt())).thenReturn(restaurant);
+        Map<String, Object> params = Map.of("rating", createSampleRatingMap());
+
+        ResponseException result = assertThrows(ResponseException.class, () -> {
+            reviewController.addReview(restaurant.getId(), params);
+        });
+
+        ResponseException expected = new ResponseException(HttpStatus.BAD_REQUEST, PARAMS_MISSING);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void addReview_RatingMissing_ThrowsException() {
         Restaurant restaurant = createSampleRestaurant();
         when(restaurantService.getRestaurant(anyInt())).thenReturn(restaurant);
         Map<String, Object> params = Map.of("comment", createSampleComment());
@@ -91,11 +137,12 @@ public class ReviewControllerTest {
         String comment = createSampleComment();
         Map<String, String> ratingMap =  Map.of("food", "", "service", "", "ambiance", "", "overall", "");
         Map<String, Object> params = Map.of("comment", comment,"rating", ratingMap);
-        when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(restaurant);
+        when(restaurantService.getRestaurant(anyInt())).thenReturn(restaurant);
 
         ResponseException result = assertThrows(ResponseException.class, () -> {
             reviewController.addReview(restaurant.getId(), params);
         });
+
         assertEquals(HttpStatus.BAD_REQUEST, result.getStatus());
         assertEquals(PARAMS_BAD_TYPE, result.getMessage());
     }
@@ -107,7 +154,7 @@ public class ReviewControllerTest {
         Map<String, Number> ratingMap = createSampleRatingMap();
         Map<String, Object> params = Map.of("comment", comment,"rating", ratingMap);
 
-        when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(restaurant);
+        when(restaurantService.getRestaurant(anyInt())).thenReturn(restaurant);
         UserNotFound exception = createSampleUserNotFoundException();
         doThrow(exception).when(reviewService).addReview(anyInt(), any(), any());
 
@@ -118,36 +165,5 @@ public class ReviewControllerTest {
         ResponseException expected = new ResponseException(HttpStatus.BAD_REQUEST, exception);
         assertEquals(expected, result);
     }
-
-    @Test
-    public void addReview_ValidParams_addsReviews() throws UserNotFound, ManagerCannotReview, UserHasNotReserved, RestaurantNotFound, InvalidReviewRating {
-        Restaurant restaurant = createSampleRestaurant();
-        String comment = createSampleComment();
-        Map<String, Number> ratingMap = createSampleRatingMap();
-        Map<String, Object> params = Map.of("comment", comment,"rating", ratingMap);
-        when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(restaurant);
-
-        reviewController.addReview(restaurant.getId(), params);
-
-        verify(reviewService).addReview(eq(restaurant.getId()), argThat(rating ->
-                        rating.food == ratingMap.get("food").doubleValue() &&
-                                rating.service == ratingMap.get("service").doubleValue() &&
-                                rating.ambiance == ratingMap.get("ambiance").doubleValue() &&
-                                rating.overall == ratingMap.get("overall").doubleValue()),
-                eq(comment));
-    }
-
-    @Test
-    public void addReview_ValidParams_ReturnsCorrectResponse() {
-        Restaurant restaurant = createSampleRestaurant();
-        String comment = createSampleComment();
-        Map<String, Number> ratingMap = createSampleRatingMap();
-        Map<String, Object> params = Map.of("comment", comment,"rating", ratingMap);
-        when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(restaurant);
-
-        Response response = reviewController.addReview(restaurant.getId(), params);
-
-        assertEquals(HttpStatus.OK, response.getStatus());
-        assertEquals("review added successfully", response.getMessage());
-    }
+    
 }
