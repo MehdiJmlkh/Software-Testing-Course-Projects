39fe781ac8cb0936dfee34b2566f8762b0e099f6
Mehdi <mehdijmlkh1382@gmail.com>
2024-11-14 19:57:41 +0330

    Clean code

--- Fesadyab/src/test/java/domain/TransactionEngineTest.java
+++ Fesadyab/src/test/java/domain/TransactionEngineTest.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static utils.CreateSample.*;
 
